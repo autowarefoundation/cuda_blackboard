da33a0d8ee5376baacbe130f97a51f9c9a64fe8e
Manato HIRABAYASHI <manato.hirabayashi@tier4.jp>
2026-06-10 18:18:03 +0900

    refactor: return const reference from const ready_event()
    Signed-off-by: Manato HIRABAYASHI <manato.hirabayashi@tier4.jp>

--- include/cuda_blackboard/cuda_pointcloud2.hpp
+++ include/cuda_blackboard/cuda_pointcloud2.hpp
@@ -38,7 +38,7 @@ public:
   ~CudaPointCloud2();
 
   cudaEvent_t & ready_event() { return *ready_event_; }
-  cudaEvent_t ready_event() const { return *ready_event_; }
+  const cudaEvent_t & ready_event() const { return *ready_event_; }
 
   CudaUniquePtr<std::uint8_t[]> data;
 
