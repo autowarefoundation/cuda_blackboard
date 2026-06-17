4a735186a971678356deccf2bf5d7ad66ff8b172
Manato HIRABAYASHI <manato.hirabayashi@tier4.jp>
2026-06-17 16:24:51 +0900

    refactor: avoid default argument and add overloads with deprecated attribute
    This detects deprecated API usage at compile time and warns about it
    
    Signed-off-by: Manato HIRABAYASHI <manato.hirabayashi@tier4.jp>

--- include/cuda_blackboard/cuda_blackboard_subscriber.hpp
+++ include/cuda_blackboard/cuda_blackboard_subscriber.hpp
@@ -20,13 +20,21 @@ class CudaBlackboardSubscriber
 public:
   [[deprecated]] CudaBlackboardSubscriber(
     rclcpp::Node & node, const std::string & topic_name, bool add_compatible_sub,
-    std::function<void(std::shared_ptr<const T>)> callback,
-    cudaStream_t user_stream = cudaStreamLegacy);
+    std::function<void(std::shared_ptr<const T>)> callback, cudaStream_t user_stream);
+
+  [[deprecated(
+    "Use the constructor that takes cudaStream_t to avoid legacy-default-stream synchronization, "
+    "which can degrade performance.")]] CudaBlackboardSubscriber(rclcpp::Node & node, const std::string & topic_name, bool add_compatible_sub, std::function<void(std::shared_ptr<const T>)> callback)
+  : CudaBlackboardSubscriber(node, topic_name, add_compatible_sub, callback, cudaStreamLegacy) {};
 
   CudaBlackboardSubscriber(
     rclcpp::Node & node, const std::string & topic_name,
-    std::function<void(std::shared_ptr<const T>)> callback,
-    cudaStream_t user_stream = cudaStreamLegacy);
+    std::function<void(std::shared_ptr<const T>)> callback, cudaStream_t user_stream);
+
+  [[deprecated(
+    "Use the constructor that takes cudaStream_t to avoid legacy-default-stream synchronization, "
+    "which can degrade performance.")]] CudaBlackboardSubscriber(rclcpp::Node & node, const std::string & topic_name, std::function<void(std::shared_ptr<const T>)> callback)
+  : CudaBlackboardSubscriber(node, topic_name, callback, cudaStreamLegacy) {};
 
 private:
   void instanceIdCallback(const std_msgs::msg::UInt64 & instance_id_msg);
